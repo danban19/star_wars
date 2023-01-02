801980e2dba9fd8c7cf37022bbab633a2486fc39
danban19 <daniel.banasik1@gmail.com>
2023-01-02 17:25:54 +0100

    improve template for listing characters

--- src/api/views.py
+++ src/api/views.py
@@ -5,5 +5,7 @@ from django.http import HttpResponse
 
 def character(request):
     r = requests.get('https://swapi.dev/api/people/')
+    for item in r.json()['results']:
+        print(item)
 
-    return render(request, 'api/characters.html', {})
+    return render(request, 'api/characters.html', {'characters': r.json()['results']})
