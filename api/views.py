5eedcc508cfd95cb54c86c3e90d56de23aac3d74
danban19 <daniel.banasik1@gmail.com>
2023-01-05 16:25:05 +0100

    implement data manipulation

--- api/views.py
+++ api/views.py
@@ -14,6 +14,7 @@ def character_view(request):
     r = requests.get('https://swapi.dev/api/people/')
     data = r.json()['results']
     headers = [item for item in next(iter(data))]
+    update_homeworld(data)
     export_to_csv(data)
 
     return render(
@@ -31,8 +32,7 @@ def collections_view(request):
 
 
 def single_collection_view(request, name):
-    table = petl.fromcsv(name)
-    petl.tojson(table)
+    table = retrieve_table_to_json(name)
     headers = [item for item in next(iter(table))]
     characters = table[1:]
     return render(request, 'api/collection.html', {'characters': characters, 'headers': headers, 'name':name})
@@ -42,10 +42,34 @@ def export_to_csv(data):
     name = f'collection_{datetime.now().strftime(("%Y-%m-%dT%H-%M-%S"))}.csv'
     save_to_db(name)
     table = petl.fromdicts(data)
+
+    # Rename 'edited' to 'date' column
+    table = petl.rename(table, 'edited', 'date')
+
+    # Remove redundant columns
+    columns_to_remove = ['films', 'species', 'vehicles', 'starships', 'created', 'url']
+    for column in columns_to_remove:
+        table = petl.cutout(table, column)
+
+    # Save table to csv file
     petl.tocsv(table, name)
+
+    return table
+
+
+def retrieve_table_to_json(name):
+    table = petl.fromcsv(name)
+    petl.tojson(table)
+    return table
 
 
 def save_to_db(name):
     file_name = Collection(name=name)
     file_name.save()
+
+
+def update_homeworld(data):
+    for character in data:
+        character['homeworld'] = requests.get(character['homeworld']).json()['name']
+
 
